362a09ce0478e9045b9fff5dc18454d9273676e1
Samaesha <65852261+Samaesha@users.noreply.github.com>
2020-09-08 21:13:42 +0530

    Add files via upload

---
+++ project 38 endless runner game - 2/js/Form.js
@@ -0,0 +1,36 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h1')
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    this.title.html("Sweet Bunny Blossom");
+    this.title.position(displayWidth/2-150, displayHeight/2-200);
+
+    this.input.position(displayWidth/2-100, displayHeight/2-110);
+    this.button.position(displayWidth/2-35, displayHeight/2-60);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2-90, 250);
+    });
+
+  }
+}
